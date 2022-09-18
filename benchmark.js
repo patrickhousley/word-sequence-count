42d161f4f025bdf683100b2b62fe60156e566515
Patrick Housley <patrick.housley1@t-mobile.com>
2022-09-18 09:43:51 -0500

    Finish first impl

--- benchmark.js
+++ benchmark.js
@@ -1,32 +1,36 @@
-const wordSequenceCounter = require('./dist/index').default
+const wordSequenceCounter = require('./dist/lib/counter').default
 
-const originalLogger = console.log
-console.log = () => {}
-console.warn = () => {}
-console.error = () => {}
-console.debug = () => {}
+;(async () => {
+  const originalLogger = console.log
+  console.log = () => {}
+  console.warn = () => {}
+  console.error = () => {}
+  console.debug = () => {}
 
-const shortStart = performance.mark('short-start')
-for (let i = 0; i < 1000; i++) {
-  wordSequenceCounter('resources/short.txt')
-}
-const shortMeasure = performance.measure('short', shortStart)
-originalLogger(`Processing resources/short.txt took ${shortMeasure.duration}ms`)
+  const shortStart = performance.mark('short-start')
+  for (let i = 0; i < 1000; i++) {
+    await wordSequenceCounter('resources/short.txt')
+  }
+  const shortMeasure = performance.measure('short', shortStart)
+  originalLogger(
+    `Processing resources/short.txt took ${shortMeasure.duration}ms`
+  )
 
-const mobyStart = performance.mark('moby-start')
-for (let i = 0; i < 1000; i++) {
-  wordSequenceCounter('resources/mobydick.txt')
-}
-const mobyMeasure = performance.measure('moby', mobyStart)
-originalLogger(
-  `Processing resources/mobydick.txt took ${mobyMeasure.duration}ms`
-)
+  const mobyStart = performance.mark('moby-start')
+  for (let i = 0; i < 1000; i++) {
+    await wordSequenceCounter('resources/mobydick.txt')
+  }
+  const mobyMeasure = performance.measure('moby', mobyStart)
+  originalLogger(
+    `Processing resources/mobydick.txt took ${mobyMeasure.duration}ms`
+  )
 
-const unicodeStart = performance.mark('moby-start')
-for (let i = 0; i < 1000; i++) {
-  wordSequenceCounter('resources/unicode.txt')
-}
-const unicodeMeasure = performance.measure('unicode', unicodeStart)
-originalLogger(
-  `Processing resources/unicode.txt took ${unicodeMeasure.duration}ms`
-)
+  const unicodeStart = performance.mark('moby-start')
+  for (let i = 0; i < 1000; i++) {
+    await wordSequenceCounter('resources/unicode.txt')
+  }
+  const unicodeMeasure = performance.measure('unicode', unicodeStart)
+  originalLogger(
+    `Processing resources/unicode.txt took ${unicodeMeasure.duration}ms`
+  )
+})()
--- src/lib/counter.ts
+++ src/lib/counter.ts
@@ -1,22 +1,92 @@
 import fs from "node:fs"
 import process from "node:process"
 
-export default function wordSequenceCounter(inputFilePath: string): void {
-  let inputStream: fs.ReadStream;
+const punctuationRegex = /[\s!\.\?,:;\[\]\(\)—¿"]/
+const wordSplitRegex = /[\s]/
+let wordCount: Map<string, number>;
+let wordCache: string[];
+let wordGroups: string[];
 
-  try {
-    inputStream = fs.createReadStream(inputFilePath);
-  } catch (error) {
-    console.error(`Could not open file ${inputFilePath} for reading`);
-    process.exit(1);
+export default async function wordSequenceCounter(inputFilePath: string): Promise<void> {
+  return new Promise((resolve, reject) => {
+    let inputStream: fs.ReadStream;
+    wordCount = new Map();
+    wordCache = [];
+    wordGroups = [];
+
+    try {
+      inputStream = fs.createReadStream(inputFilePath);
+    } catch (error) {
+      console.error(`Could not open file ${inputFilePath} for reading`);
+      return reject()
+    }
+
+    inputStream.on('error', (error: Error): void => {
+      console.error(`An error occurred processing file`, error);
+      return reject()
+    })
+
+    inputStream.on('data', (chunk: Buffer): void => {
+      dataHandler(chunk);
+    })
+
+    inputStream.on('close', () => {
+      writeResults();
+      resolve();
+    })
+  })
+}
+
+function dataHandler(chunk: Buffer): void {
+  let word = wordCache;
+  chunk.forEach((charCode: number, index: number): void => {
+    const char = String.fromCharCode(charCode).toLowerCase();
+
+    // If a space, process the word
+    if (wordSplitRegex.test(char)) {
+      processWord(word.join(""));
+      word = [];
+      return;
+    }
+
+    // Remove punctuation
+    if (punctuationRegex.test(char)) {
+      return;
+    }
+
+    word.push(char);
+
+    if (index === chunk.length - 1) {
+      wordCache = word;
+    }
+  })
+}
+
+function processWord(word: string) {
+  if (word.trim() === '') {
+    return;
   }
 
-  inputStream.on('error', (error) => {
-    console.error(`An error occurred processing file`, error);
-    process.exit(1);
-  })
+  wordGroups.push(word);
 
-  inputStream.on('data', (chunk) => {
-    console.log(chunk);
-  })
+  if (wordGroups.length === 3) {
+    const wordGroup = wordGroups.join(" ");
+    wordGroups = [];
+
+    if (wordCount.has(wordGroup)) {
+      wordCount.set(wordGroup, wordCount.get(wordGroup) as number + 1);
+    } else {
+      wordCount.set(wordGroup, 1);
+    }
+  }
+}
+
+function writeResults(): void {
+  const sortedWordCount = [...wordCount]
+    .sort((wordCountGroupA: [string, number], wordCountGroupB: [string, number]): number => wordCountGroupB[1] - wordCountGroupA[1])
+
+  fs.writeFileSync('results.json', JSON.stringify(sortedWordCount));
+  sortedWordCount.slice(0, 3).forEach((entry: [string, number]): void => {
+    console.log(`${entry[0]} -> ${entry[1]}`);
+  });
 }
